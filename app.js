395bf0c0a92a09ce493823079a4cfcc569ef78ce
Kelly Raila <kelly.raila@gmail.com>
2015-10-07 14:19:26 -0400

    create comments template with default comments

--- app.js
+++ app.js
@@ -37,7 +37,13 @@ function($scope, posts){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0});
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong...', upvotes: 0}
+      ]
+    });
+
     $scope.title = '';
     $scope.link = '';
   };
@@ -51,5 +57,6 @@ function($scope, posts){
 '$stateParams',
 'posts',
 function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
 
 }]);
